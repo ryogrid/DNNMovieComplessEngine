968ca1275743ade9af6a02398b750632c1c38dc7
Ryo Kanbayashi <ryo.contact@gmail.com>
2018-01-26 12:35:29 +0900

    fashin-MNIST introduced

--- conv_autoencoder_sample.py
+++ conv_autoencoder_sample.py
@@ -4,6 +4,7 @@
 
 from keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D, concatenate
 from keras.models import Model, Sequential
+import mnist_reader
 
 model = Sequential()
 
@@ -28,8 +29,11 @@ model.compile(optimizer='adam', loss='binary_crossentropy')
 from keras.datasets import mnist
 import numpy as np
 
-(x_train, _), (x_test, _) = mnist.load_data()
 
+x_train, y_train = mnist_reader.load_mnist('data/fashion', kind='train')
+x_test, y_test = mnist_reader.load_mnist('data/fashion', kind='t10k')
+
+# (x_train, _), (x_test, _) = mnist.load_data()               
 x_train = x_train.astype('float32') / 255.
 x_test = x_test.astype('float32') / 255.
 x_train = np.reshape(x_train, (len(x_train), 28, 28, 1))
@@ -37,7 +41,7 @@ x_test = np.reshape(x_test, (len(x_test), 28, 28, 1))
 
 model.fit(x_train, x_train,
                 #nb_epoch=50,
-                nb_epoch=5,
+                nb_epoch=1,
                 batch_size=128,
                 shuffle=True,
                 validation_data=(x_test, x_test))
